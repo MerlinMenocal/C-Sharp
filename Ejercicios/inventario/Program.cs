824521008ad3062353a43d1765b5ac1db9deb93a
Merlin Menocal <merlinmm0530@gmail.com>
2022-06-20 23:10:41 -0600

    agregando inventario

---
+++ Ejercicios/inventario/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+ namespace inventario
+ {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+
+            while (true)
+            {
+                console.Clear();
+                console.WriteLine("SISTEMA DE INVENTARIO");
+                console.WriteLine("*********************");
+                console.WriteLine("");
+                console.WriteLine("1- Productos");
+                console.WriteLine("2- Ingreso de Inventario");
+                console.WriteLine("3- Salida de Inventario");
+                console.WriteLine("0- Salir");
+                opcion = console.Read();
+
+                switch (opcion)
+                {
+                    case 1:
+                      console.WriteLine("Productos!");
+                      console.Read(); //sirve para hacer pausa
+                      //listaProductos();
+                      break;
+                    
+                    default:
+                }
+                
+                if (opcion ==0)
+                {
+                    break;
+                }
+
+            }
+            
+        }
+    }
+ }
