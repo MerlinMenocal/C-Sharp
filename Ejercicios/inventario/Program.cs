c129e5dd45e384e6f9e2b3e1ab2e0b4a70652141
Merlin Menocal <merlinmm0530@gmail.com>
2022-06-22 11:54:06 -0600

    cambios 1.3

--- Ejercicios/inventario/Program.cs
+++ Ejercicios/inventario/Program.cs
@@ -4,6 +4,29 @@
  {
     class Program
     {
+        //[,]divide cuantas columnas y filas
+        string[,] productos = new string[5,3]
+        {
+            {"001", "IphoneX", "0"},
+            {"002","Table Samsumg A8","100"},
+            {"003","Laptop Dell","85"},
+            {"004","CPU Dell","20"},
+            {"005","Monitor HP","24"}
+        };
+        
+         void listaProductos(){
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("Listado de Productos");
+            Console.WriteLine("********************");
+            Console.WriteLine("Codigo, Descripcion y Existencia");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(productos[i,0] + " | " + productos[i, 1] + " | " + productos[i,2]);
+            }
+         }
+
         static void Main(string[] args)
         {
             string opcion ="";
@@ -22,7 +45,7 @@
 
                 switch (opcion)
                 {
-                    case "1"
+                    case "1":
                       Console.WriteLine("Productos!");
                       Console.Read(); //sirve para hacer pausa
                       //listaProductos();
