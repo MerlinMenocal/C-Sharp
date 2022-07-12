deb011745497d431aaa4054f3853800ecacbc7d6
Merlin Menocal <merlinmm0530@gmail.com>
2022-07-12 12:01:20 -0600

    cambios

--- Ejercicios/propiedades/Alumno.cs
+++ Ejercicios/propiedades/Alumno.cs
@@ -1,6 +1,13 @@
 
 public class Alumno 
 {
-  
+  public int Id {get; set;}
+
+  public string PrimerNombre {get; set;}
+
+  public string SegundoNombre {get; set;}
+
+
+
 
 }
--- Ejercicios/propiedades/Program.cs
+++ Ejercicios/propiedades/Program.cs
@@ -2,3 +2,13 @@
 Console.WriteLine("Hello, World!");
 
 Alumno a = new Alumno();
+a.Id= 1;
+a.PrimerNombre = "Juan";
+a.SegundoNombre = "Perez";
+
+Alumno b =new Alumno();
+b.Id = 2;
+b.PrimerNombre = "Maria";
+b.SegundoNombre = "Martinez";
+
+Console.WriteLine(a.PrimerNombre);
