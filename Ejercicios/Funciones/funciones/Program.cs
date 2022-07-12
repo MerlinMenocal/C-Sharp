1f5ee5ff246934415b1885802ef2b19ee45ab922
Merlin Menocal <merlinmm0530@gmail.com>
2022-07-12 12:54:20 -0600

    cambio 2

--- Ejercicios/Funciones/funciones/Program.cs
+++ Ejercicios/Funciones/funciones/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-Alumno a = new Alumno(, "Juan", "Perez");
+Alumno a = new Alumno(1, "Juan", "Perez");
 
 Console.WriteLine(a.nombreCompleto());
 
