a567efdb53c053571cab1675b1da1822e7b14a87
Merlin Menocal <merlinmm0530@gmail.com>
2022-07-12 18:23:51 -0600

    cambio 1

--- Ejercicios/foreach/listados/Program.cs
+++ Ejercicios/foreach/listados/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Collection.Generic;
+
+List<string> nombres = new List<string>();
+nombres.Add("Juan");
+nombres.Add("Pedro");
+nombres.Add("Elizabeth");
+
+foreach (var nombre in nombres)
+{
+    Console.WriteLine(nombre);
+}
+
+List<int> numeros = new List<int>();
+numeros.Add(10);
+numeros.Add(1);
+numeros.Add(5);
+
+foreach (var numero in numeros)
+{
+    Console.WriteLine(numero);
+}
+
