bd2d6a5addeb5a0ba54b7bcb6d7f084fd2ae8355
Merlin Menocal <merlinmm0530@gmail.com>
2022-07-25 18:53:32 -0600

    agregando ejercicio de ordenes

---
+++ Ejercicios/10-Ordenes/DatosdePrueba.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+public class DatosdePrueba 
+{
+    public List<Producto> ListadeProductos {get; set;}
+
+    public DatosdePrueba()
+    {
+        ListadeProductos = new list<Producto>();
+        cargarProductos();
+    }
+
+    
+    private void cargarProductos()
+    {
+        Producto a = new Producto(1, "Mouse", 250)
+        ListadeProductos.Add(a);
+
+        Producto b = new Producto(2, "Monitor", 4600)
+        ListadeProductos.Add(b);
+
+        Producto c = new Producto(3, "Teclado", 800)
+        ListadeProductos.Add(c);
+
+    }
+
+    public void listarProductos()
+    {
+        Console.clear();
+        Console.WriteLine("Lista de Productos");
+        Console.WriteLine("------------------");
+        Console.WriteLine("");
+        foreach (var producto in ListadeProductos)
+        {
+            Console.WriteLine(producto.Codigo + " | " + producto.Descripcion + " | " + producto.Precio);
+        }
+    }
+}
---
+++ Ejercicios/10-Ordenes/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+DatosdePrueba datos = new DatosdePrueba();
+
+string opcion = "";
+
+while (true)
+{
+    Console.clear();
+    Console.WriteLine("Sistema de Ordenes");
+    Console.WriteLine("------------------");
+    Console.WriteLine("");
+    Console.WriteLine("1 - Lista de Productos");
+    Console.WriteLine("2 - Crear Orden");
+    Console.WriteLine("3 - Lista de Clientes");
+    Console.WriteLine("4 - Lista de Vendedores");
+    Console.WriteLine("5 - Lista de Ordenes");
+    Console.WriteLine("0 - Salir");
+    opcion = Console.ReadLine();
+
+    switch (opcion)
+    {
+        case "1":
+         datos.listarProductos();
+         break;
+
+        default:
+    }
+
+
+    if(opcion == "0")
+    {
+        break;
+    }
+
+
+
+
+
+}
+
+
