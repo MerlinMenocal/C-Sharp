90ca33f05d94a0d7ca3d648a8255b8c6a7a148a6
Merlin Menocal <merlinmm0530@gmail.com>
2022-08-03 21:55:42 -0600

    cambios 1.1

--- Ejercicios/Pilares-POO/ListaAnimales.cs
+++ Ejercicios/Pilares-POO/ListaAnimales.cs
@@ -4,15 +4,15 @@ using System.Collections.Generic;
 public class ListaAnimales {
 
     public List<Animales> ListadeAnimales {get; set;}
-    // public List<Mamiferos> ListadeMamiferos {get; set;} 
+    public List<Mamiferos> ListadeMamiferos {get; set;} 
 
     public ListaAnimales()
     {
      ListadeAnimales = new List<Animales>();
      encontrarAnimales();
      
-    //  ListadeMamiferos = new List<Mamiferos>();
-    //  caminar();
+     ListadeMamiferos = new List<Mamiferos>();
+     //caminar();
 
  
     }
@@ -20,13 +20,13 @@ public class ListaAnimales {
  //metodos
  private void encontrarAnimales(){
 
-    Animales a = new Animales("Oso", "cafe", 150, "cafes", "Comida");
+    Animales a = new Animales("Oso", "cafe", 150, "cafes");
     ListadeAnimales.Add(a);
 
-    Animales b = new Animales("Elefante", "Gris", 300, "Negros", "Comida");
+    Animales b = new Animales("Elefante", "Gris", 300, "Negros");
     ListadeAnimales.Add(b);
 
-     Animales c = new Animales("Tigre", "Negro", 140, "Negros", "Comida");
+     Animales c = new Animales("Tigre", "Negro", 140, "Negros");
     ListadeAnimales.Add(c);
  }
 
@@ -43,8 +43,11 @@ public void listarAnimales()
             Console.WriteLine("Color: " + Animales.Color);
             Console.WriteLine("Peso en Kg: " + Animales.Peso);
             Console.WriteLine("Color de Ojos: " + Animales.ColorOjos);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Animales.Comer();
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("=====================");
-            Comer();
+            
                  
         }
 
@@ -53,32 +56,33 @@ public void listarAnimales()
 
 //     private void caminar(){
 
-//     Mamiferos a = new Mamiferos("Oso", "cafe", 150, "cafes", "Comida");
+//     Mamiferos a = new Mamiferos("Oso", "cafe", 150, "cafes");
 //     ListadeMamiferos.Add(a);
 
-//     Mamiferos b = new Mamiferos("Oso", "cafe", 150, "cafes", "Comida");
+//     Mamiferos b = new Mamiferos("Oso", "cafe", 150, "cafes");
 //     ListadeMamiferos.Add(b);
 //    }
 
-//  public void listarMamiferos()
-//     {
-//         Console.Clear();
-//         Console.WriteLine("Lista de Animales");
-//         Console.WriteLine("-----------------");
-//         Console.WriteLine("");
-//         foreach (var mamiferos in ListadeMamiferos)
-//         {
-//             Console.WriteLine("Animal: " + mamiferos.Nombre);
-//             Console.WriteLine("Color: " + mamiferos.Color);
-//             Console.WriteLine("Peso en Kg: " + mamiferos.Peso);
-//             Console.WriteLine("Color de Ojos: " + mamiferos.ColorOjos);
-//             Console.WriteLine("Alimentacion: " + mamiferos.patas);
-//             Console.WriteLine("=====================");
+ public void listarMamiferos()
+    {
+        Console.Clear();
+        Console.WriteLine("Lista de Animales");
+        Console.WriteLine("-----------------");
+        Console.WriteLine("");
+        foreach (var mamiferos in ListadeMamiferos)
+        {
+            // Console.WriteLine("Animal: " + mamiferos.Nombre);
+            // Console.WriteLine("Color: " + mamiferos.Color);
+            // Console.WriteLine("Peso en Kg: " + mamiferos.Peso);
+            // Console.WriteLine("Color de Ojos: " + mamiferos.ColorOjos);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Mamiferos.Caminar();
+            Console.ForegroundColor = ConsoleColor.White;
                  
-//         }
+        }
 
-//         Console.ReadLine();
-//     }
+        Console.ReadLine();
+    }
 }
 
 
--- Ejercicios/Pilares-POO/mamiferos.cs
+++ Ejercicios/Pilares-POO/mamiferos.cs
@@ -1,25 +1,24 @@
-// namespace Animal{
-
-//     public class Mamiferos :Animales
-//      {   
-
-//         public string Patas{get; set; }
 
 
+using System;
+using System.Collections.Generic;
 
-//          public string Patas (int patas){
-//             Patas = patas;
-//          }
+    public class Mamiferos :Animales
+     {   
+        public string Patas{get; set; }
 
-//     // public Mamiferos(string nombre, string color, double peso, string colorojos, string patas)
-//     // {
-//     //     Nombre = nombre;
-//     //     Color = color;
-//     //     Peso = peso;
-//     //     ColorOjos = colorojos;
         
-//     //  }
-//     public void  Caminar(){
-//         Caminar();
-//     }
-// }
+    public Mamiferos(string pata)
+    {
+        // string nombre, string color, double peso, string colorojos
+        // Nombre = nombre;
+        // Color = color;
+        // Peso = peso;
+        // ColorOjos = colorojos;
+        Patas = patas;
+        
+    }
+    public void  Caminar(){
+        Console.WriteLine("Este animal Camina");
+    }
+}
