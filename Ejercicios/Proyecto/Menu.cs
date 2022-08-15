dc0cfec5ef140e9de5d3eedb632541e8a5e51ff6
Merlin Menocal <merlinmm0530@gmail.com>
2022-08-15 15:58:46 -0600

    cambios 1.1

--- Ejercicios/Proyecto/Menu.cs
+++ Ejercicios/Proyecto/Menu.cs
@@ -1,50 +1,105 @@
 ﻿using System;
-Login Log = new Login();
-string opcion = "";
+using System.Collections.Generic;
 
-while (true)
+class Menu
 {
-    //Console.BackgroundColor = ConsoleColor.Blue;
-    Console.Clear();
-    Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine("          Por Favor Seleccione su Transacción");
-    Console.WriteLine("       ________________________________________");
-    Console.WriteLine("");
-    Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine("       | 1 - Retirar Dinero                   |");
-    Console.WriteLine("       | 2 - Ver Saldo Disponible             |");
-    Console.WriteLine("       | 3 - Historial de Transacciones       |");
-    Console.ForegroundColor = ConsoleColor.White;
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine("       | 0 - Salir                            |");
-    Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine("       ________________________________________");
-    Console.ForegroundColor = ConsoleColor.White;
-    opcion = Console.ReadLine();
-
-    switch (opcion)
+    static void Main(string[] args)
     {
-    case "1":
-    
-    break;
+        Login Log = new Login();
 
-    case "2":
-    
-    break;
+        string pinacceso = "";
+        int contador = 1;
+            
+        while(true)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("        CAJERO AUTOMATICO");
+            Console.WriteLine("      _____________________");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.Write("      Ingrese su Token:");
+            pinacceso = Console.ReadLine();
 
-    case "3":
-    
-    break;
+            bool resultado = Log.Validar(pinacceso);
 
-    default:
-    break;
+            if(resultado == true){
+                Console.Clear();
+                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("             ============");
+                Console.WriteLine("            | BIENVENID@ |");
+                Console.WriteLine("             ============");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("");
+                Console.WriteLine("Precione una tecla para continuar...");
+                Console.ReadKey();
+    //-------------------------------------------------------------------------------------------            
+            string opcion = "";
+
+            while (true)
+            {
+                //Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("          Por Favor Seleccione su Transacción");
+                Console.WriteLine("       ________________________________________");
+                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("       | 1 - Retirar Dinero                   |");
+                Console.WriteLine("       | 2 - Ver Saldo Disponible             |");
+                Console.WriteLine("       | 3 - Historial de Transacciones       |");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("       | 0 - Salir                            |");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("       ________________________________________");
+                Console.ForegroundColor = ConsoleColor.White;
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                case "1":
+                
+                break;
+
+                case "2":
+                
+                break;
+
+                case "3":
+                
+                break;
+
+                default:
+                break;
+                }
+
+                if(opcion == "0")
+                {
+                    break;
+                } 
+            }
+            break;
+        } 
+        else{
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("");
+                Console.WriteLine("       Código incorrecto!");
+                Console.WriteLine("           Intento " + contador + "/3");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadKey();
+            }
+            contador ++;
+            if(contador  > 3){
+                return;
+            }
+        }  
     }
-
-
-    if(opcion == "0")
-    {
-        break;
-    }
-
 }
+
+
+
+
 
